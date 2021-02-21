f842d5d2ab5efbc3ef0b7f59225f88567020be23
anggakswr <222180023@student.unaki.ac.id>
2021-02-21 16:05:44 +0700

    Add background to Nav component Add new index.js file

--- components/Nav.js
+++ components/Nav.js
@@ -1,16 +1,21 @@
 const Nav = () => {
   return (
-    <div className="flex justify-between items-center p-5">
-      <div className="text-3xl">Rosok.com</div>
+    <div className="flex justify-between items-center p-5 bg-green-500">
+      <a href="#" className="text-3xl link-white">
+        Rosok.com
+      </a>
+
       <input
         type="text"
-        className="border-2 w-screen py-1 px-2 mx-10"
+        className="border w-screen py-1 px-3 mx-16"
         placeholder="Cari barang rosok"
       />
-      <a href="#" className="mx-3">
+
+      <a href="#" className="mx-3 link-white">
         Login
       </a>
-      <a href="#" className="mx-3">
+
+      <a href="#" className="mx-3 link-white">
         Daftar
       </a>
     </div>
